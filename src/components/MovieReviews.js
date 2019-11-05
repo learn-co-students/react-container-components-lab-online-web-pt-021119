80c1c0d01e9710af386a911d789135f2a98298f9
cyantis <cyantis@gmail.com>
2019-11-05 12:26:48 -0700

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,14 @@
-// Code MovieReviews Here
+import React from 'react';
+
+const MovieReviews = (props) => {
+  let movieList = [];
+  for(let r of props.moviesArr){
+    movieList.push(
+        <p className="review">Title: { r.display_title } | Critics pick? { r.critics_pick ? 'YES' : 'NO' }</p>
+    )
+  }
+
+  return(<div className="review-list">{ movieList }</div>)
+}
+
+export default MovieReviews;
